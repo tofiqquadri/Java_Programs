18f490dd1915927431d7f64e8669bc9d57c6fe02
Tofiq Quadri <Tofiq Quadri@LAPTOP-ETGCV0EA>
2016-08-08 19:14:38 +0530

    new Commit

---
+++ src/MultipleMain.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tofiq Quadri
+ */
+public class MultipleMain {
+    
+    public static void main(int a[])
+    {    
+      System.out.println("Main method 0");
+    }
+    
+    public static void main(String ar[])
+    {    
+      System.out.println("Main method 1");
+    }
+    
+    public static void main(double arg[])
+    {    
+      System.out.println("Main method 2");
+    }
+    
+    public static void main(float args[])
+    {    
+      System.out.println("Main method 3");
+    }
+}
